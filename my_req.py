7ce704908781ec864af4c9f9a2558699cc28d98e
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-07-25 09:47:25 +0530

    some changes

--- my_req.py
+++ my_req.py
@@ -3,7 +3,7 @@ import json
 url = 'http://saral.navgurukul.org/api/courses'
 # by writing function we write file Name file Data
 def writingFile(fileName,fileData):
-      file = open(fileName,"wb")
+      file = open(fileName,"w")
       dumps_data = json.dumps(fileData)
       file.write(dumps_data)
       file.close()
@@ -40,7 +40,7 @@ course_id = course_id_list[user_input-1]
 def api2Calling(url2):
       get_exercise_data = requests.get(url2)
       dict_data = get_exercise_data.json() # dict type data 
-      jsonFileData="excersice"+str(course_id)+".json"
+      jsonFileData="exerciseFolder/excersice"+str(course_id)+".json"
 
       #write second url data by calling writingfile functon 
       exercise_data=writingFile(jsonFileData,dict_data)
@@ -50,22 +50,22 @@ def api2Calling(url2):
       inside_course_data = load_exercise["data"]
       i = 0
       for course in inside_course_data:
-            print i+1,course["name"],course["id"]
+            print i+1,course["name"],course["id"],course["slug"]
             inside_childexercise_data = course["childExercises"]
-            j=0
-            for index in inside_childexercise_data:
-                  print  "\t",j+1,index["name"],index["id"]
-                  j=j+1
-
-                  # inside_exercise_slug = index["slug"]
-                  # a = 0
-                  # for exercise in inside_exercise_slug:
-                  #       print "\t",a+1,exercise
+            if inside_childexercise_data == []:
+                  print "[]"
+            else:
+                  j=0
+                  for index in inside_childexercise_data:
+                        print  "\t",j+1,index["name"],index["id"],index["slug"]
+                        j=j+1
+                  # a= 0
+                  # for index in inside_childexercise_data:
+                  #       print "\t","\t",a+1,index["slug"]
                   #       a = a+1
 
             i=i+1
 url2 = "http://saral.navgurukul.org/api/courses/"+str(course_id)+"/exercises"
 api2Calling(url2)
-print url2
 
-url_2 = "http://saral.navgurukul.org/course?id=18&slug=python__WhatDoComputersDo"
+#url_2 = "http://saral.navgurukul.org/course?id=18&slug=python__WhatDoComputersDo"
